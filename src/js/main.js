97c4e07c51198da6f3ba095407527a914483a39c
Everett Tsosie <tsosie.everett@gmail.com>
2023-01-24 16:37:26 -0700

    update main

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
+import { Linter } from 'eslint';
 import ProductData from './ProductData.js';
 import ProductList from './ProductList.mjs';
 
 const productList = new ProductData("tents");
+const list = new ProductList(category, dataSource, listElement); // change params later
+
+
