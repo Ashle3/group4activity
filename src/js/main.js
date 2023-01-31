f5e4c72aee25c7924835b571ed5ea7c7960b61da
mckennahpalmer <mkpalm2006@gmail.com>
2023-01-31 16:15:37 -0700

    team activity 4

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,11 @@ import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 import { loadHeaderFooter } from './utils.mjs';
 
+//the code for loadHeaderFooter
+loadHeaderFooter();
+
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const list = new ProductList("tents", dataSource, element); // change params later
-
-//the code for loadHeaderFooter
-loadHeaderFooter("./src/index.html");
-//loadHeaderFooter("./product_pages/index.html")
-// loadHeaderFooter("./cart/index.html");
-// loadHeaderFooter("./src/index.html");
-// loadHeaderFooter("./src/checkout.html"); //may need a checkout js file for this.
 
 list.init();
