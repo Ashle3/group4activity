e3d3f3738988ddac953a5e54049f11d93edfed08
mckennahpalmer <mkpalm2006@gmail.com>
2023-01-31 17:05:13 -0700

    team activity 4

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,33 +1,36 @@
 import { getLocalStorage } from "./utils.mjs";
 import { loadHeaderFooter } from './utils.mjs';
+import ShoppingCart from "./ShoppingCart.mjs";
 
 //the code for loadHeaderFooter
 loadHeaderFooter();
 
+const cart = new ShoppingCart("so-cart", ".product-list");
+cart.renderCartContents();
 
-function renderCartContents() {
-  const cartItems = [getLocalStorage("so-cart")];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-}
+// function renderCartContents() {
+//   const cartItems = [getLocalStorage("so-cart")];
+//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+// }
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+// function cartItemTemplate(item) {
+//   const newItem = `<li class="cart-card divider">
+//   <a href="#" class="cart-card__image">
+//     <img
+//       src="${item.Image}"
+//       alt="${item.Name}"
+//     />
+//   </a>
+//   <a href="#">
+//     <h2 class="card__name">${item.Name}</h2>
+//   </a>
+//   <p class="cart-card__color">${item.Colors}</p>
+//   <p class="cart-card__quantity">qty: 1</p>
+//   <p class="cart-card__price">$${item.FinalPrice}</p>
+// </li>`;
 
-  return newItem;
-}
+//   return newItem;
+// }
 
-renderCartContents();
+// renderCartContents();
