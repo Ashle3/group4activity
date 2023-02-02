5a5b412c759b9d864589eeebfd481e9ff20fbd59
mckennahpalmer <mkpalm2006@gmail.com>
2023-02-02 15:30:22 -0700

    team activity 4

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,8 +1,4 @@
 import { setLocalStorage } from './utils.mjs';
-import { loadHeaderFooter } from './utils.mjs';
-
-//the code for loadHeaderFooter
-loadHeaderFooter();
 
 function productDetailsTemplate(product) {
     return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
