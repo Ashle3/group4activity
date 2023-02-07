3ac5b8732fde1d8d303c9d60d44b59f5f1ed1eee
Ashle3 <ashleeb3@byui.edu>
2023-02-07 17:05:22 -0600

    starting stretch goals

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,12 +1,13 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
-import { loadHeaderFooter } from './utils.mjs';
+import { loadHeaderFooter, getParam } from './utils.mjs';
 
 //the code for loadHeaderFooter
 loadHeaderFooter();
 
+const category = getParam("category");
 const dataSource = new ProductData();//Find out what to pass here
 const element = document.querySelector(".product-list");
-const list = new ProductList("tents", dataSource, element); // change params later
+const list = new ProductList(category, dataSource, element); // change params later
 
 list.init();
