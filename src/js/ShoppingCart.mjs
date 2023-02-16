844a4fcb0682c77f8ef9b78889d9fcbb91c4f9da
mckennahpalmer <mkpalm2006@gmail.com>
2023-02-16 16:30:47 -0700

    Checkout form

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -21,7 +21,17 @@ function cardItemTemplate(product) {
 export default class ShoppingCart {
   constructor(key, parentSelector){
       this.key = key;
-      this.parentSelector = parentSelector;        
+      this.parentSelector = parentSelector;
+      this.total = 0;      
+  }
+  async init() {
+    const list = getLocalStorage(this.key);
+    this.calculateListTotal(list);
+    this.renderCartContents(list);
+  }
+  calculateListTotal(list) {
+    const amounts = list.map((item) => item.FinalPrice);
+    this.total = amounts.reduce((sum, item) => sum + item);
   }
   renderCartContents(){
     const cartItems = getLocalStorage(this.key);
