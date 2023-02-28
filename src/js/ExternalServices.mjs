f182a52c589cc13b1f48bc725f1b037d37f07db5
Ashle3 <ashleeb3@byui.edu>
2023-02-28 17:41:57 -0600

    finished team 8 activity

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,4 +1,5 @@
-const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+//const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+const baseURL = "https://wdd330-backend.vercel.app/";
 async function convertToJson(res) {
   const jsonResponse = await res.json();
   if (res.ok) {
@@ -35,5 +36,35 @@ export default class ExternalServices {
     // const response = await fetch(baseURL + "checkout", options);
     // const data = await convertToJson(response);
     // return data.Result;
+  }
+
+  async loginRequest(userInfo){
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    };
+    const loginVariable = await fetch(baseURL + "login", options).then(convertToJson);
+    return loginVariable.accessToken;
+    //{ email: "user1@email.com" , password: "user1" }
+  }
+
+  // make a request to the server for the current orders
+  // requires: a valid token
+  // returns: a list of orders
+  async getOrders(token) {
+    const options = {
+      method: "GET",
+      // the server will reject our request if we don't include the Authorization header with a valid token!
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(baseURL + "orders", options).then(
+      convertToJson
+    );
+    return response;
   }
 }
